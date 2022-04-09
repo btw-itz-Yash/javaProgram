334f74db3a68dd5b3ac407505f73f0d6d802c966
Yash Gangwar <yashgangwar.ind@gmail.com>
2022-04-09 17:45:40 +0530

    Print Fibonacci sequence till nth term using Recursion.

--- src/dsaPracticeQuestion/Recursion/Recursion5.java
+++ src/dsaPracticeQuestion/Recursion/Recursion5.java
@@ -7,9 +7,7 @@ public class Recursion5 {
         }
         int c = a+b;
         System.out.print(c+" ");
-        a = b;
-        b = c;
-        printFib(a, b, n-1);
+        printFib(b, c, n-1);
     }
     public static void main(String[] args) {
         int a = 0, b = 1;
