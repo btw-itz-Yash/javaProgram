a6b4cb7cde77f20d5701df570ba8b1c0523624dc
Yash Gangwar <yashgangwar.ind@gmail.com>
2022-04-09 16:44:26 +0530

    Print Number 5 to 1 using Recursion

---
+++ src/dsaPracticeQuestion/Recursion/PrintNumber5to1.java
@@ -0,0 +1,15 @@
+package dsaPracticeQuestion.Recursion;
+
+public class PrintNumber5to1 {
+    public static void printNum(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        printNum(n);
+    }
+}
